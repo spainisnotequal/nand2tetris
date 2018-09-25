c536eaa228047307ae4e278a7dee63fccf1ecb81
spainisnotequal <spainisnotequal@gmail.com>
2018-09-25 11:03:00 +0200

    Add "I/O-Handling Program"

--- project_04/fill/Fill.asm
+++ project_04/fill/Fill.asm
@@ -11,4 +11,47 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+	@SCREEN
+	D=A
+	@word
+	M=D	// word = first word of the screen's memory
+
+	@KBD
+	D=M	// input status of the keyboard
+	@WHITE
+	D;JEQ	// if D = 0, then go to WHITE
+	@BLACK
+	D;JNE	// if D != 0, then go to BLACK
+
+(WHITE)
+	@colour
+	M=0	// set the colour variable to white("0" decimal in 16-bits binary is "0000000000000000")
+	@DRAW
+	0;JMP	// go to DRAW
+
+(BLACK)
+	@colour
+	M=-1	// set the colour variable to black ("-1" decimal in 16-bits binary is "1111111111111111")
+	@DRAW
+	0;JMP	// go to DRAW
+
+(DRAW)
+	@word
+	D=M	// current word address
+	@KBD
+	D=D-A	// D = current word address - keyboard address
+	@START
+	D;JEQ	// if D = 0  (current word address is equal to the keyboard address), go to START and start the program again
+	
+	@colour
+	D=M
+	@word
+	A=M
+	M=D	// set that word to zeros or ones depending on the colour variable
+
+	@word
+	M=M+1	// next word
+
+	@DRAW
+	0;JMP	// go to DRAW and continue drawing the next word
